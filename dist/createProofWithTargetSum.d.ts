7e05d670e6d4d6b18bbeb8c58ee2829b85434b08
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-01-25 21:35:40 +0100

    build

--- dist/createProofWithTargetSum.d.ts
+++
@@ -1,2 +0,0 @@
-import { MerkleProofWithTargetSum, Node } from './types';
-export default function createProofWithTargetSum(index: number, targetSum: bigint, depth: number, arity: number, nodes: Node[][], zeroes: Node[], root: Node): MerkleProofWithTargetSum;
--- dist/verifyProofWithTargetSum.d.ts
+++
@@ -1,2 +0,0 @@
-import { HashFunction, MerkleProofWithTargetSum } from './types';
-export default function verifyProof(merkleProofWithTargetSum: MerkleProofWithTargetSum, hash: HashFunction): boolean;

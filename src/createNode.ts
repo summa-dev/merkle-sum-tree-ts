68d5356ed2fe4ef22f149b4df942968fb36754c0
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-02-02 12:43:55 +0100

    removed `createLeafNodeFromEntry` from createNode function

--- src/createNode.ts
+++ src/createNode.ts
@@ -1,16 +1,4 @@
-import { HashFunction, Node, Entry } from './types';
-import Utils from './utils';
-
-export function createLeafNodeFromEntry(entry: Entry, hash: HashFunction): Node {
-  if (entry.balance < BigInt(0)) {
-    throw new Error('entrySum cant be negative');
-  }
-  const hashPreimage: bigint[] = [Utils.parseUsername(entry.username), entry.balance];
-
-  const leaf: Node = { hash: hash(hashPreimage), sum: entry.balance };
-
-  return leaf;
-}
+import { HashFunction, Node } from './types';
 
 export function createMiddleNode(childL: Node, childR: Node, hash: HashFunction): Node {
   const middleNode = { hash: hash([childL.hash, childL.sum, childR.hash, childR.sum]), sum: childL.sum + childR.sum };

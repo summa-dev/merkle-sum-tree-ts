1f58c3136e558105ca9acab611aec2382e0654ef
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-02-02 12:41:41 +0100

    added entry as independent class

---
+++ src/entry.ts
@@ -0,0 +1,45 @@
+import { poseidon } from 'circomlibjs'
+import { Node } from './types'
+import Utils from './utils'
+
+export default class Entry {
+    usernameToBigInt: bigint;
+    balance: bigint;
+
+  constructor(usernameToBigInt : bigint, balance : bigint) {
+
+    if (balance < BigInt(0)) {
+        throw new Error('entry balance cant be negative');
+      }
+
+    this.usernameToBigInt = usernameToBigInt;
+    this.balance = balance;
+
+    // Freeze the object to prevent any changes
+    Object.freeze(this);
+  }
+
+  public getLeafHash() : Node {
+
+        const hashPreimage: bigint[] = [this.usernameToBigInt, this.balance];
+      
+        const leaf: Node = { hash: poseidon(hashPreimage), sum: this.balance };
+      
+        return leaf;
+    
+    }
+
+    // add getters here
+    public getBalance() : bigint {
+        return this.balance;
+    }
+
+    public getUsername() : bigint {
+        return this.usernameToBigInt;
+    }
+
+    public getStringifiedUsername() : string {
+        return Utils.stringifyUsername(this.usernameToBigInt)
+    }
+
+}

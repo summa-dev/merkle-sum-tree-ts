913fadbbfb59bc28b3dd436e9ac2eb5fbde4534e
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-02-03 16:45:41 +0100

    update testing with new naming

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,17 @@
-import { IncrementalMerkleSumTree, MerkleProof } from '../src';
+import { MerkleSumTree, MerkleProof } from '../src';
 import Entry from '../src/entry';
 
 describe('Incremental Merkle Sum Tree', () => {
-  let tree: IncrementalMerkleSumTree;
+  let tree: MerkleSumTree;
 
   beforeEach(() => {
     const pathToCsv = 'test/entries/entry-16-valid.csv';
-    tree = new IncrementalMerkleSumTree(pathToCsv);
+    tree = new MerkleSumTree(pathToCsv);
   });
 
   it('Should not initialize a tree if the csv contains invalid balance type', () => {
     const pathToInvalidCsv = 'test/entries/entry-16-invalid-balance-type.csv';
-    const fun1 = () => new IncrementalMerkleSumTree(pathToInvalidCsv);
+    const fun1 = () => new MerkleSumTree(pathToInvalidCsv);
     expect(fun1).toThrow('Balance must be a number');
   });
 
@@ -24,7 +24,7 @@ describe('Incremental Merkle Sum Tree', () => {
   it('Should not allow to initialize a tree with at least a negative balance', () => {
     // build invalid tree
     const pathToInvalidCsv = 'test/entries/entry-16-neg-balance.csv';
-    const fun = () => new IncrementalMerkleSumTree(pathToInvalidCsv);
+    const fun = () => new MerkleSumTree(pathToInvalidCsv);
 
     expect(fun).toThrow('entry balance cant be negative');
   });
@@ -32,7 +32,7 @@ describe('Incremental Merkle Sum Tree', () => {
   it('Should generate different root hashes when changing the entry order', () => {
     const pathToCsvWithSwitchedOrder = 'test/entries/entry-16-valid-switched-order.csv';
 
-    let tree2 = new IncrementalMerkleSumTree(pathToCsvWithSwitchedOrder);
+    let tree2 = new MerkleSumTree(pathToCsvWithSwitchedOrder);
 
     expect(tree.root.hash).not.toEqual(tree2.root.hash);
   });
@@ -41,8 +41,8 @@ describe('Incremental Merkle Sum Tree', () => {
     const pathToCsvWith17Entries = 'test/entries/entry-17-valid.csv';
     const pathToCsvWith15Entries = 'test/entries/entry-15-valid.csv';
 
-    const tree17 = new IncrementalMerkleSumTree(pathToCsvWith17Entries);
-    const tree15 = new IncrementalMerkleSumTree(pathToCsvWith15Entries);
+    const tree17 = new MerkleSumTree(pathToCsvWith17Entries);
+    const tree15 = new MerkleSumTree(pathToCsvWith15Entries);
 
     expect(tree17.depth).toEqual(5);
     expect(tree15.depth).toEqual(4);
@@ -50,7 +50,7 @@ describe('Incremental Merkle Sum Tree', () => {
 
   it('Should generate a tree filled up with zero entries node if the number of entries provided in the csv file is not a power of two', () => {
     const pathToCsvWith17Entries = 'test/entries/entry-17-valid.csv';
-    const tree17 = new IncrementalMerkleSumTree(pathToCsvWith17Entries);
+    const tree17 = new MerkleSumTree(pathToCsvWith17Entries);
 
     // should have a zero leaf from index 17 to 31
     for (let i = 17; i < 32; i += 1) {
@@ -68,7 +68,7 @@ describe('Incremental Merkle Sum Tree', () => {
     it(`Should generate a tree with the correct total sum starting from ${numberOfEntries[i]} leaves and verify a proof`, () => {
       const pathTocsv = `test/entries/entry-${numberOfEntries[i]}-valid.csv`;
 
-      const tree2 = new IncrementalMerkleSumTree(pathTocsv);
+      const tree2 = new MerkleSumTree(pathTocsv);
 
       const proof: MerkleProof = tree2.createProof(0);
 
